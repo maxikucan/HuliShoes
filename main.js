7f2ff3600a42591176742c1c671a35ca83393e8a
Maximiliano <maximilianokucan@gmail.com>
2021-09-07 22:47:04 -0300

    Boton comprar escucha evento.

---
+++ main.js
@@ -0,0 +1,11 @@
+// BOTON COMPRAR
+
+let comprarBtn = document.getElementById("buyBtn");
+
+comprarBtn.addEventListener("click", comprar);
+
+function comprar(){
+    alert("Estas por comprar un articulo");
+}
+
+// BUSQUEDA
