621f49d9fd2ce5b4a29050be73e2192eb15a659e
Maximiliano <maximilianokucan@gmail.com>
2021-09-17 19:07:06 -0300

    PAGINA ESTATICA FUNCIONAL. Falta carrito d compras

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 // BOTON COMPRAR
 
-let comprarBtn = document.getElementById("buyBtn");
+function comprar(){ 
+    location.href = "compra.html";
 
-comprarBtn.addEventListener("click", comprar);
+}
+// REALIZAR COMPRA
 
-function comprar(){
-    alert("Estas por comprar un articulo");
+function realizarCompra(){
+    alert("Gracias por adquirir su producto");
 }
 
-// BUSQUEDA
+function volver(){
+    location.href = "index.html"
+}
+
+const botonCompra = document.getElementById("comprador");
+let texto = document.getElementById("probanding")
+
+botonCompra.addEventListener("click", () =>{
+    texto.textContent = "Probandoo";
+})
+
+// SCROLL REVEAL
+
+ScrollReveal().reveal('.container-fluid', {delay: 100} );
+ScrollReveal().reveal('.articulos', {delay: 250} );
+ScrollReveal().reveal('#zapas-topper', {delay: 200} );
+ScrollReveal().reveal('.compra', {delay: 150} );
